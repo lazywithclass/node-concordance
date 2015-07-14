9ffee2a43b63db925787aa913654f6326942a782
lazywithclass <montecristoh@gmail.com>
2015-07-14 22:47:40 +0000

    index that could be run from the command line with pipes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = function() {
-  return 'hi';
-};
+var concordance = require('./lib/concordance'),
+    printer = require('./lib/printer');
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(data) {
+  // remove the "enter" pressed by user
+  data = data.trim();
+  printer.print(concordance.parse(data));
+});
